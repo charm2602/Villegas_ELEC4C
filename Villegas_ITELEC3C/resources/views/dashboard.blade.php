c62e79dbcbea8aaf60df4cec4517dffdcfdcc130
syarls <charlesdavereyes18@gmail.com>
2023-11-08 21:16:37 +0800

    new commit

---
+++ Villegas_ITELEC3C/resources/views/dashboard.blade.php
@@ -0,0 +1,26 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+        <table>
+                    <thead>
+                        <tr>
+                          <th scope="col">ID</th>
+                          <th scope="col">NAME</th>
+                          <th scope="col">EMAIL</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($users as $user)
+                        <tr>
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+    </x-slot>
+
+</x-app-layout>
