e7aeb9627db325985324cdcf956e92418194ff18
charm2602 <118341084+charm2602@users.noreply.github.com>
2023-12-28 10:24:37 +0800

    last

--- Villegas_ITELEC3C/resources/views/category/category.blade.php
+++ Villegas_ITELEC3C/resources/views/category/category.blade.php
@@ -13,7 +13,7 @@
                     <button class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
             @endif
-            <div class="bg-white col-md-12 overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <table class="table table-striped-columns">
                     <thead>
                         <tr>
@@ -22,41 +22,51 @@
                           <th scope="col">CATEGORY NAME</th>
                           <th scope="col">USER ID</th>
                           <th scope="col">CREATED AT</th>
-                          <th></th>
+                          <th scope="col">ACTIONS</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($categories as $category)
                         <tr>
-                            <td>{{ $categories->firstItem()+$loop->index }}</td>
+                            <td>{{ $categories->firstItem() + $loop->index }}</td>
                             <td><img width="100" height="100" alt="{{ $category->category_img }}" src="{{ asset('storage/' . $category->category_img) }}" /></td>
                             <td>{{ $category->category_name }}</td>
                             <td>{{ $category->user->name }}</td>
                             <td>{{ $category->created_at->diffForHumans() }}</td>
-                            <td class="grid gap-2">
-                                <a href="{{url('category/edit/'.$category->id)}}" class="btn btn-primary">Edit</button>
-                                <a href="{{url('category/delete/'.$category->id)}}" class="btn btn-danger">Delete</button>
+                            <td>
+                                <div class="btn-group" role="group">
+                                    <a href="{{ url('category/edit/'.$category->id) }}" class="btn btn-primary">Edit</a>
+                                    <a href="{{ url('category/delete/'.$category->id) }}" class="btn btn-danger">Delete</a>
+                                </div>
                             </td>
                         </tr>
                         @endforeach
                     </tbody>
                 </table>
-                {{$categories->links()}}
+                <div class="pagination">
+                    {{ $categories->links() }}
+                </div>
             </div>
-            <h1>Add Category</h1>
-            <div class="bg-white col-md-4 overflow-hidden shadow-xl sm:rounded-lg">  
-                <form action="{{ route('Create') }}" method="post" enctype="multipart/form-data">
-                    @csrf
-                    <div>
-                    <label for="category_name">Category Name</label>
-                    <input type="text" class="form-control" id="category_name" name="category_name" />
-                    <input type="file" name="category_img" />  
-                    @error('category_name')
-                        <span class="text-danger">{{$message}}</span>
-                    @enderror
-                    </div>
-                    <button type="submit" class="btn">Submit</button>
-                </form>
+
+            <div class="mt-4">
+                <h1 class="text-center">Add Category</h1>
+                <div class="bg-white col-md-4 overflow-hidden shadow-xl sm:rounded-lg mx-auto">
+                    <form action="{{ route('Create') }}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="category_name" class="form-label">Category Name</label>
+                            <input type="text" class="form-control" id="category_name" name="category_name" />
+                            @error('category_name')
+                                <span class="text-danger">{{$message}}</span>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="category_img" class="form-label">Category Image</label>
+                            <input type="file" class="form-control" id="category_img" name="category_img" />
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
--- Villegas_ITELEC3C/resources/views/category/edit.blade.php
+++ Villegas_ITELEC3C/resources/views/category/edit.blade.php
@@ -1,27 +1,46 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Category</title>
 
-    <div class="py-12">
-        <div class="max-w-7xl row mx-auto sm:px-6 lg:px-8">
-            <h1>Edit Category</h1>
-            <div class="bg-white col-md-4 overflow-hidden shadow-xl sm:rounded-lg">
-                <form action="{{ url('category/update/'.$categories->id) }}" method="post" enctype="multipart/form-data">
-                    @csrf
-                    <div>
-                    <label for="category_name">Category Name</label>
-                    <input type="text" class="form-control" id="category_name" name="category_name" value="{{$categories->category_name}}" /> 
-                    <input type="file" name="category_img" />  
-                    @error('category_name')
-                        <span class="text-danger">{{$message}}</span>
-                    @enderror
-                    </div>
-                    <button type="submit" class="btn">Submit</button>
-                </form>
+
+</head>
+<body>
+
+    <x-app-layout>
+
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <h1 class="text-center">Edit Category</h1>
+                <div class="card bg-white overflow-hidden shadow-xl sm:rounded-lg mx-auto">
+                    <form action="{{ url('category/update/'.$categories->id) }}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <br><br>
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="category_name">Category Name</label>
+                                <input type="text" class="form-control" id="category_name" name="category_name" value="{{$categories->category_name}}" />
+                                <input type="file" name="category_img" class="mt-2 form-control"/>
+                                @error('category_name')
+                                    <span class="text-danger">{{$message}}</span>
+                                @enderror
+                            </div>
+                            <button type="submit" id ="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-</x-app-layout>
+    </x-app-layout>
+
+   <style>
+
+    
+
+    </style>
+
+</body>
+</html>
